7b7c3226090740eff2f90c662d62c89eb52c3666
jalexss <ja.soledads@gmail.com>
2023-03-18 15:13:29 -0400

    implementando upload de imagenes para recetas

--- resources/views/recetas/images.blade.php
+++ resources/views/recetas/images.blade.php
@@ -2,23 +2,36 @@
 
 @section('content')
 <div class="container">
-    <form method="POST" class="needs-validation" action="{{ route('recetas') }}" novalidate>
+    <form method="POST" class="needs-validation" action="{{ route('recetas.updateImages', $receta->id) }}" enctype="multipart/form-data">
         @csrf
+        @method('PATCH')
+        <div class="mb-3">
+            <label for="formFileMultiple" class="form-label @error('images') is-invalid @enderror" >Multiple files input example</label>
+            <input 
+                class="form-control" 
+                type="file" 
+                id="formFileMultiple" 
+                multiple
+                name="images[]"
+            >
+            @error('images')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+            @if (count($errors) > 0)
+                <div >
 
-        <div class="row mb-3">
-            <label for="title" class="form-label">{{ __('Title')}}</label>
-            <div id="titleHelp" class="col-md-6 form-text">
-                <input type="text" name="title" class="form-control @error('title') is-invalid @enderror" id="titleInput" value="{{ old('title') }}" required autofocus>
-                {{ __('Try a nice title for you receta!. (max: 70 characters)') }}
-
-                @error('title')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-            </div>
+                    <strong>Whoops!</strong> There were some problems with your input.
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif 
         </div>
-        <button type="submit" class="btn btn-primary">Create</button>
+        <button type="submit" class="btn btn-primary">Save</button>
     </form>
 </div>
 @endsection
