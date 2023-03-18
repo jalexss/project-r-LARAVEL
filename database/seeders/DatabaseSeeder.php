6e2844661782c2213f3de17dd6973cab53413b75
jalexss <ja.soledads@gmail.com>
2023-03-18 15:01:32 -0400

    agregando creacion del profile de user - DBSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,10 +17,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
+        $user = User::factory()->create([
             'username' => 'alex',
             'email' => 'alex@prueba.com',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password 
         ]);
+
+        Profile::factory()
+            ->count(1)
+            ->for($user)
+            ->create();
     }
 }
